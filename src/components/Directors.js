0c6b44f421a67721430477f39314d5738ed07c68
ian-ramos <ramos.ianc@gmail.com>
2017-11-03 12:08:50 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorsArr = directors.map(director => {
+    return (
+      <div>
+        <h3>{director.name}</h3>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsArr}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const moviesArr = movies.map(movie => {
+    return (
+      <div>
+        <h3>{movie.title}</h3>
+        <p>{movie.time} minutes</p>
+        <p>{movie.metascore} from Metacritic</p>
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {moviesArr}
     </div>
   );
 };
